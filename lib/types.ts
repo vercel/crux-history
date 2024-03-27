5ba8007c5da6f3385d725960943ef2f836adbf22
Phil Zona <phil.zona@vercel.com>
2024-03-27 17:59:51 -0400

    Clean up

--- lib/types.ts
+++ lib/types.ts
@@ -51,23 +51,3 @@ export type CruxChart = {
     backgroundColor: string;
   }[];
 }
-
-
-
-// datasets: [
-//   {
-//     label: 'Dataset 1',
-//     data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//     backgroundColor: 'rgb(255, 99, 132)',
-//   },
-//   {
-//     label: 'Dataset 2',
-//     data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//     backgroundColor: 'rgb(75, 192, 192)',
-//   },
-//   {
-//     label: 'Dataset 3',
-//     data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//     backgroundColor: 'rgb(53, 162, 235)',
-//   },
-// ],
